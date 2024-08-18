2811bacd325fdb9b10e66ec2fa054a42977a71e6
khj6775 <khj6775@hotmail.com>
2024-08-18 22:50:55 +0900

    20240818

--- keras/keras63_성우하이텍.py
+++ keras/keras63_성우하이텍.py
@@ -15,13 +15,18 @@ print(x2_datasets.shape)    # (948, 17)
 x3_datasets = pd.read_csv('C:\\ai5\\_data\\중간고사데이터\\성우하이텍 240816.csv', index_col=0, thousands = ',')
 print(x3_datasets.shape)    # (7058, 17)
 
-from sklearn.preprocessing import LabelEncoder
+x1_datasets = x1_datasets.drop(['전일비', '신용비', '개인', '기관', '외인(수량)', '외국계', '프로그램', '외인비'], axis = 1)
+x2_datasets = x2_datasets.drop(['전일비', '신용비', '개인', '기관', '외인(수량)', '외국계', '프로그램', '외인비'], axis = 1)
+x3_datasets = x3_datasets.drop(['전일비', '신용비', '개인', '기관', '외인(수량)', '외국계', '프로그램', '외인비'], axis = 1)
 
-le = LabelEncoder()
 
-x1_datasets['전일비'] = le.fit_transform(x1_datasets['전일비'])
-x2_datasets['전일비'] = le.fit_transform(x2_datasets['전일비'])
-x3_datasets['전일비'] = le.fit_transform(x3_datasets['전일비'])
+# from sklearn.preprocessing import LabelEncoder
+
+# le = LabelEncoder()
+
+# x1_datasets['전일비'] = le.fit_transform(x1_datasets['전일비'])
+# x2_datasets['전일비'] = le.fit_transform(x2_datasets['전일비'])
+# x3_datasets['전일비'] = le.fit_transform(x3_datasets['전일비'])
 
 
 # size = 5
@@ -35,7 +40,13 @@ def split_x(data, size):
 x1_datasets = split_x(x1_datasets, 5)
 x2_datasets = split_x(x2_datasets, 5)
 x3_datasets = split_x(x3_datasets, 5)
-y = split_x(x3_datasets, 5)
+y = split_x(x3_datasets, 1)
+
+
+x1_datasets = x1_datasets[:944]
+x2_datasets = x2_datasets[:944]
+x3_datasets = x3_datasets[:944]
+y = y[:944]
 
 
 
@@ -45,10 +56,16 @@ print(x3_datasets.shape)
 print(y.shape)      # (7054, 5)
 # exit()
 
-x1_datasets = x1_datasets[:944]
-x2_datasets = x2_datasets[:944]
-x3_datasets = x3_datasets[:944]
-y = y[:944]
+# x1_datasets = x1_datasets.values.astype(np.float)
+# x2_datasets = x2_datasets.values.astype(np.float)
+# x3_datasets = x3_datasets.values.astype(np.float)
+
+y = np.array(x3_datasets[:, 3]) # 성우하이텍 종가
+
+
+x1_datasets = x1_datasets[::-1]
+x2_datasets = x2_datasets[::-1]
+x3_datasets = x3_datasets[::-1]
 
 # x_test1 = x3_datasets[-1].reshape(-1,5,16)  # 맨 마지막 x 로 평가  -1 = 맨마지막줄
 # # print(x)
@@ -58,21 +75,27 @@ y = y[:944]
 # x = x[ :143, : ]  # 인덱싱
 # y = y[1:144, : ]
 
+# y = y[1:]
+
+x1_pred = x1_datasets[-1:]
+x2_pred = x2_datasets[-1:]
+x3_pred = x3_datasets[-1:]
 
 x1_train, x1_test, x2_train, x2_test, x3_train, x3_test, y_train, y_test = train_test_split(x1_datasets, x2_datasets, x3_datasets, y,
-                                                                         train_size=0.8, random_state= 215)
+                                                                         train_size=0.8, random_state= 722)
 
 print(x1_train.shape, )
 print(x2_train.shape, )
 print(x3_train.shape,)
+# y_train = y_train.reshape(-1,5*5,16)
 print(y_train.shape,)
 # exit()
 
 #2-1. 모델
-input1 = Input(shape=(16,1))
-dense1 = Conv1D(filters=10, kernel_size=2)(input1)
-dense2 = (Conv1D(10, 2))(dense1)
-dense3 = (Flatten())(dense2)
+input1 = Input(shape=(5,8))
+dense1 = Conv1D(filters=10, kernel_size=2,padding='same')(input1)
+dense2 = Conv1D(10, 2,padding='same')(dense1)
+# dense3 = (Flatten())(dense1)
 
 dense4 = Dense(64, activation='relu', name='bit2')(dense2)
 dense5 = Dense(32, activation='relu', name='bit3')(dense4)
@@ -82,26 +105,26 @@ model1 = Model(inputs=input1, outputs=output1)
 # model1.summary()
 
 #2-2. 모델
-input11 = Input(shape=(16,1))
-dense11 = Conv1D(filters=10, kernel_size=2)(input11)
-dense12 = (Conv1D(10, 2))(dense11)
+input11 = Input(shape=(5,8))
+dense11 = Conv1D(filters=10, kernel_size=2, padding='same')(input11)
+dense12 = Conv1D(10, 2,padding='same')(dense11)
 # dense13 = (Flatten())(dense12)
 dense14 = Dense(32, activation='relu', name='bit21')(dense12)
 output11 = Dense(16, activation='relu', name='bit31')(dense14)
 model2 = Model(inputs=input11, outputs=output11)
 
 #2-3. 모델
-input2 = Input(shape=(16,1))
-dense21 = Conv1D(filters=10, kernel_size=2 )(input2)
-dense22 = (Conv1D(10, 2))(dense21)
-dense32 = (Flatten())(dense22)
-dense33 = Dense(32, activation='relu', name='bit210')(dense32)
+input2 = Input(shape=(5,8))
+dense21 = Conv1D(filters=10, kernel_size=2 , padding='same')(input2)
+dense22 = Conv1D(10, 2,padding='same')(dense21)
+# dense32 = (Flatten())(dense22)
+dense33 = Dense(32, activation='relu', name='bit210')(dense22)
 output111 = Dense(16, activation='relu', name='bit310')(dense33)
 model3 = Model(inputs=input2, outputs=output111)
 
 
 
-#2-3. 합체!!!
+#2-4. 합체!!!
 # merge1 = concatenate([output1, output11], name='mg1')
 merge1 = Concatenate(name='mg1')([output1, output11, output111])
 merge1 = Flatten()(merge1)
@@ -146,8 +169,8 @@ mcp = ModelCheckpoint(
 )
 
 model.fit([x1_train, x2_train, x3_train], y_train,
-          epochs=3000,
-          batch_size=128,
+          epochs=1000,
+          batch_size=32,
           validation_split=0.2,
           callbacks=[es,mcp])
 
@@ -155,10 +178,10 @@ model.fit([x1_train, x2_train, x3_train], y_train,
 loss = model.evaluate([x1_test, x2_test, x3_test], y_test, verbose=1)
 print('loss : ', loss)
 
-y_pre1 = np.array([range(100,105), range(301,306)]).T
-y_pre2 = np.array([range(201,206), range(411,416), range(150,155)]).T
-y_pre3 = np.array([range(90,95), range(110,115),range(120,125),range(130,135)]).T
+# y_pre1 = np.array([range(100,105), range(301,306)]).T
+# y_pre2 = np.array([range(201,206), range(411,416), range(150,155)]).T
+# y_pre3 = np.array([range(90,95), range(110,115),range(120,125),range(130,135)]).T
 
-y_pre = model.predict([y_pre1, y_pre2, y_pre3])
+y_pre = model.predict([x1_pred, x2_pred, x3_pred])
 
-print("3101~3105 까지 나와라\n", y_pre)
+print("종가 : ", y_pre)
