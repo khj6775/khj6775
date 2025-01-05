ee9f71fc66539280d517027c6025762fe19ebe06
khj6775 <khj6775@hotmail.com>
2025-01-05 10:32:22 +0900

    20250105

--- tf114/tf10_loss_weights.graph.py
+++ tf114/tf10_loss_weights.graph.py
@@ -9,7 +9,25 @@ w = tf.compat.v1.placeholder(tf.float32)
 
 hypothesis = x * w
 
-loss = tf.reduce_mean(tf.squre(hypothesis - y))   # mse
+loss = tf.reduce_mean(tf.square(hypothesis - y))   # mse
 
 w_history = []
-loss_
+loss_history = []
+
+with tf.compat.v1.Session() as sess:
+    for i in range (-30, 50):
+        curr_w = i
+        curr_loss = sess.run(loss, feed_dict={w:curr_w})
+
+        w_history.append(curr_w)
+        loss_history.append(curr_loss)
+
+print("================= Loss history ====================")
+print(loss_history)
+
+plt.plot(w_history, loss_history)
+plt.xlabel('Weights')
+plt.ylabel('Loss')
+plt.grid()
+plt.show()
+
